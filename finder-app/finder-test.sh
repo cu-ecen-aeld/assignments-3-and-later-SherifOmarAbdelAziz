c0be1419b100510a4fac150851d25348365f0714
SherifOmarAbdelaziz <sherif.omar.abdelaziz.2013@gmail.com>
2023-12-28 20:57:16 +0200

    Fix test

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -32,9 +32,9 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+assignment=$(cat ../conf/assignment.txt)
 
-if [ $assignment != 'assignment1' ]
+if [ "$assignment" = "assignment1" ]
 then
 	mkdir -p "$WRITEDIR"
 
@@ -57,7 +57,7 @@ do
 	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(./finder-app.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
